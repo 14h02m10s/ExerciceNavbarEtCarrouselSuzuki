0469f8ec460331d6a93fec12caf94fee8fdeb411
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-20 15:11:12 +0200

    modifs nuxt.config.js etc pour deploiement

---
+++ dist/_nuxt/eee2a7d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{293:function(n,e){},300:function(n,e,t){"use strict";var f=t(293),c=t.n(f);e.default=c.a},303:function(n,e,t){"use strict";t.r(e);var f=t(47),c=t(300),component=Object(f.a)({},undefined,undefined,!1,null,null,null);"function"==typeof c.default&&Object(c.default)(component),e.default=component.exports}}]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  target: 'static',
+  router: {
+    base: '/ExerciceNavbarEtCarrouselSuzuki/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'fred-project',
